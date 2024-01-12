36cdd5ede3f95c419762de625cfa9054f45b5c7b
venkigowda11 <venkateshmnvenki@gmail.com>
2024-01-12 13:51:24 +0530

    minor

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,11 @@ import store from "../redux/store";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }) {
-  return <div>Helooo</div>;
-  // return (
-  //   <Provider store={store}>
-  //     <Layout>
-  //       <Component {...pageProps} />
-  //     </Layout>
-  //   </Provider>
-  // );
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  );
 }
